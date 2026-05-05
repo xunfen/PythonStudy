e327d7e6c8d8bf27fe164893da1bc23b0b3a87d8
xunfen <107894831+xunfen@users.noreply.github.com>
2026-05-05 17:15:37 +0800

    Add files via upload

---
+++ Day01/Demo4.py
@@ -0,0 +1,27 @@
+num = 10
+print(num)
+print(type(num))
+
+num = 100
+print(num)
+
+num = 10.1
+print(num)
+print(type(num))
+
+num = "Not Num"
+print(num)
+print(type(num))
+
+print("========================")
+
+print(type(10))
+print(type(10.1))
+print(type("sb"))
+print(type(True))
+print(type(None))   #NoneType类型
+
+print("========================")
+
+print(isinstance(10, int))  #判断变量是否为指定类型,是否返回True
+print(isinstance(10.1, int))#判断变量是否为指定类型,否返回False
---
+++ Day02/Demo1.py
@@ -0,0 +1,20 @@
+#双引号定义
+s1 = "hello world"
+print(s1)
+
+#单引号定义
+s2 = 'hello world'
+print(s2)
+
+#三引号定义(多行字符串)
+s3 = """第一行
+第二行hello world
+第三行"""
+print(s3)
+
+s4  = '''hello 
+world'''
+print(s4)
+
+s5 = 'It\'s a good day' #单引号中不能有单引号，如果要出现则需使用转义符\
+print(s5)
