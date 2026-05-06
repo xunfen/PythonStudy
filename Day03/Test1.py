0e259d579a961b34e990e3b19c067524ea63e7aa
xunfen <107894831+xunfen@users.noreply.github.com>
2026-05-06 22:15:49 +0800

    Add files via upload

---
+++ Day03/Test1.py
@@ -0,0 +1,6 @@
+username = input("请输入账号: ")
+password = input("请输入密码: ")
+if username != "admin" or password != "123456":
+    print(f"用户{username}登录失败！")
+if username == "admin" and password == "123456":
+    print(f"用户{username}登录成功！")
